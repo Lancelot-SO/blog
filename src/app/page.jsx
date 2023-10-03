99e39989ad115c2492a24eb2c602a9d99681c698
Lancelot-SO <fsowah001@gmail.com>
2023-10-03 12:05:39 +0000

    designed home page and footer section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styles from "./page.module.css";
+import hero from "public/hero.png";
+import Image from "next/image";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products.
+        </h1>
+        <p className={styles.desc}>
+          Turning your idea into reality. We bring together the teams from the
+          global tech industry.
+        </p>
+        <button className={styles.button}>See My works</button>
+      </div>
+      <div className={styles.item}></div>
+      <Image src={hero} alt="hero" className={styles.img} />
+    </div>
+  );
 };
 
 export default Home;
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import Styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>
+    <div className={Styles.container}>
       <div>©2023 Lancelot. All rights reserved.</div>
-      <div></div>
+      <div className={Styles.social}>
+        <Image
+          src="/facebook.png"
+          width={15}
+          height={15}
+          className={Styles.icon}
+          alt="facebook"
+        />
+        <Image
+          src="/instagram.png"
+          width={15}
+          height={15}
+          className={Styles.icon}
+          alt="instagram"
+        />
+        <Image
+          src="/3.png"
+          width={15}
+          height={15}
+          className={Styles.icon}
+          alt="twitter"
+        />
+        <Image
+          src="/4.png"
+          width={15}
+          height={15}
+          className={Styles.icon}
+          alt="youtube"
+        />
+      </div>
     </div>
   );
 };
